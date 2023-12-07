dce6046dfeb472dc332ea69878baa261c368d28c
Mahendra <mahendrabrowny@gmail.com>
2023-12-07 17:45:31 +0530

    style: theming fixes

--- src/components/University.jsx
+++ src/components/University.jsx
@@ -26,7 +26,7 @@ const UniversitySearch = () => {
 
     return () => clearTimeout(debounceTimer);
   }, [debouncedSearch]);
-
+  console.log("cccc ", country);
   const calculateCounts = (data) => {
     // highest and lowest counts
     const counts = data.reduce((acc, university) => {
@@ -56,30 +56,48 @@ const UniversitySearch = () => {
   };
 
   return (
-    <div>
-      <h1>University Search</h1>
-      <label>
-        Enter Country:
-        <input type="text" value={country} onChange={handleInputChange} />
-      </label>
-      <div>
-        <h2>Total Universities: {universities.length}</h2>
-        <h3>
-          Highest Count: {highestCount} ({countryWithHighestCount})
-        </h3>
-        <h3>
-          Lowest Count: {lowestCount} ({countryWithLowestCount})
-        </h3>
+    <div className="w-full px-6 py-3 flex flex-col items-center justify-cente shadow-md">
+      <h1 className="text-3xl font-bold mb-4 text-white">
+        University Search 🏫
+      </h1>
+      <div className="flex flex-col">
+        <label className="text-zinc-200">Enter Country:</label>{" "}
+        <input
+          type="text"
+          placeholder="enter country name..."
+          value={country}
+          onChange={handleInputChange}
+          className="border rounded-lg border-gray-300 p-2 text-black w-full"
+        />
       </div>
-      <ul>
-        {universities.map((university, index) => (
-          <li key={index}>
-            <strong>{university.name}</strong> ({university.country})
-            <br />
-            Website: {university.web_pages[0]}
-          </li>
-        ))}
-      </ul>
+      <div className="my-4">
+        <h2 className="text-xl text-zinc-200 font-bold mb-2">
+          Total Universities in {country}:{" "}
+          <span className="text-indigo-300">{universities.length}</span>
+        </h2>
+        <p className="text-lg text-gray-400">Highest Count: {highestCount}</p>
+        <p className="text-lg text-gray-400">Lowest Count: {lowestCount}</p>
+      </div>
+      <div className="flex items-center justify-center">
+        <ul className="flex items-start flex-col justify-center">
+          {universities.map((university, index) => (
+            <li key={index} className="mb-4 text-indigo-200">
+              {index + 1}.&nbsp;<strong>{university.name}</strong> (
+              {university.country})
+              <br />
+              <span className="italic font-light">Website: </span>
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href={university.web_pages[0]}
+                className="text-blue-500"
+              >
+                {university.web_pages[0]}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
