b4a821857b7fd4f93413cd476d7dd352ee4f8238
Mahendra <mahendrabrowny@gmail.com>
2023-12-07 18:09:16 +0530

    fix: show info text for no results

--- src/components/University.jsx
+++ src/components/University.jsx
@@ -8,10 +8,15 @@ const UniversitySearch = () => {
   const [countryWithHighestCount, setCountryWithHighestCount] = useState("");
   const [countryWithLowestCount, setCountryWithLowestCount] = useState("");
   const [debouncedSearch, setDebouncedSearch] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [noData, setNoData] = useState(false);
 
   useEffect(() => {
     const debounceTimer = setTimeout(() => {
+      // Fetch universities based on the country
       if (debouncedSearch) {
+        setLoading(true);
+        setNoData(false);
         fetch(
           `http://universities.hipolabs.com/search?country=${debouncedSearch}`
         )
@@ -19,14 +24,32 @@ const UniversitySearch = () => {
           .then((data) => {
             setUniversities(data);
             calculateCounts(data);
+            setLoading(false);
+            if (data.length === 0) {
+              setNoData(true);
+            }
           })
-          .catch((error) => console.error("Error fetching data:", error));
+          .catch((error) => {
+            console.error("Error fetching data:", error);
+            setLoading(false);
+            setNoData(true);
+          });
+      } else {
+        setUniversities([]);
+        setHighestCount(0);
+        setLowestCount(0);
+        setCountryWithHighestCount("");
+        setCountryWithLowestCount("");
+        setLoading(false);
+        setNoData(false);
       }
     }, 500);
 
     return () => clearTimeout(debounceTimer);
   }, [debouncedSearch]);
+
   console.log("cccc ", country);
+
   const calculateCounts = (data) => {
     // highest and lowest counts
     const counts = data.reduce((acc, university) => {
@@ -78,24 +101,41 @@ const UniversitySearch = () => {
         <p className="text-lg text-gray-400">Highest Count: {highestCount}</p>
         <p className="text-lg text-gray-400">Lowest Count: {lowestCount}</p>
       </div>
-      <div className="flex items-center justify-center">
+      {loading && (
+        <p className="text-lg text-white font-bold mb-4">Loading...</p>
+      )}
+      {noData && !loading && (
+        <p className="text-lg text-white font-bold my-8">
+          No results found for {debouncedSearch}
+        </p>
+      )}
+      {!loading && !noData && universities.length === 0 && (
+        <p className="text-lg text-white mt-11">
+          Enter a country name to search for universities.
+        </p>
+      )}
+      <div className="flex items-center flex-col justify-center">
+        {universities?.length && (
+          <h2 className="text-white text-4xl mb-6">Search Results 👇🏻</h2>
+        )}
         <ul className="flex items-start flex-col justify-center">
-          {universities.map((university, index) => (
-            <li key={index} className="mb-4 text-indigo-200">
-              {index + 1}.&nbsp;<strong>{university.name}</strong> (
-              {university.country})
-              <br />
-              <span className="italic font-light">Website: </span>
-              <a
-                target="_blank"
-                rel="noreferrer"
-                href={university.web_pages[0]}
-                className="text-blue-500"
-              >
-                {university.web_pages[0]}
-              </a>
-            </li>
-          ))}
+          {universities.length > 0 &&
+            universities.map((university, index) => (
+              <li key={index} className="mb-4 text-indigo-200">
+                {index + 1}.&nbsp;<strong>{university.name}</strong> (
+                {university.country})
+                <br />
+                <span className="italic font-light">Website: </span>
+                <a
+                  target="_blank"
+                  rel="noreferrer"
+                  href={university.web_pages[0]}
+                  className="text-blue-500"
+                >
+                  {university.web_pages[0]}
+                </a>
+              </li>
+            ))}
         </ul>
       </div>
     </div>
