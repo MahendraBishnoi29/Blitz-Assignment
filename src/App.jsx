7033ca891bb5358c1420cf31e62edda908fd4734
Mahendra <mahendrabrowny@gmail.com>
2023-12-08 09:34:53 +0530

    config: react router dom with final changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import UniversitySearch from "./components/University";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import AllUniversities from "./components/AllUniversities";
+import UniversitySearch from "./components/SearchUniversity";
 
 function App() {
   return (
-    <div className="h-full">
-      <UniversitySearch />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<UniversitySearch />} />
+        <Route path="/all-universities" element={<AllUniversities />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
